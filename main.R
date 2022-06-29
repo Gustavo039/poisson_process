0686ec6bd909888bcedd87ebb58102f78a7cf411
Gustavo Almeida Silva <gustavoaeida2002@gmail.com>
2022-06-29 18:32:39 -0300

    initial commit

---
+++ main.R
@@ -0,0 +1,35 @@
+poi.process <- function(lambda,n){
+  
+  # initialize vector of total wait time for the arrival of each event:
+  s<-numeric(n+1)
+  # set S_0 = 0
+  s[1] <-0
+  # generate vector of iid Exp random variables:
+  x <-replicate(n,rexp(1,lambda))
+  # assign wait time to vector s in for loop:
+  for (k in 1:n){
+    
+    s[k+1] <-sum(x[1:k])
+    
+  }
+  # return vector of wait time
+  return(s)
+  
+}
+
+n<-20
+
+lambda <-3
+
+# simulate list of wait time:
+s_list <-poi.process(lambda,n)
+
+# plot function:
+plot(stepfun(0:(n-1), s_list), 
+     do.points = TRUE,
+     pch = 16,
+     col.points = "red",
+     verticals = FALSE,
+     main = 'Realization of a Poisson process with lambda = 3',
+     xlab = 'Time of arrival',
+     ylab = 'Number of arrivals')
